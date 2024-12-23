53b5b31e9d8a80a64389a6108ad060d4643adb4d
sudedonmezz <sudednmz13@gmail.com>
2024-12-24 00:51:21 +0300

    Login

--- Store/HairArt/Controllers/AccountController.cs
+++ Store/HairArt/Controllers/AccountController.cs
@@ -1,10 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using HairArt.Models;
+
 namespace HairArt.Controllers;
 
 public class AccountController : Controller
 {
+    private readonly UserManager<IdentityUser> _userManager;
+
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+    {
+        _signInManager=signInManager;
+        _userManager = userManager;
+    }
     public IActionResult Login()
     {
+        return View();
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> Login([FromForm] LoginModel model)
+    {
+        if(ModelState.IsValid)
+        {
+            IdentityUser user = await _userManager.FindByEmailAsync(model.Email);
+
+            if(user is not null)
+            {
+                await _signInManager.SignOutAsync();
+                if((await _signInManager.PasswordSignInAsync(user,model.Password,false,false)).Succeeded)
+                {
+                    return Redirect("/");
+                }
+            }
+
+            ModelState.AddModelError("Error","Invalid email or password.");
+        }
         return View();
     }
 }
